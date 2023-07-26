d7b209688e775c5ce8ed9e0171dd5ee7d5db3ed4
gusca <gusca083@student.liu.se>
2023-07-26 11:59:21 +0200

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '461bab71f8mshcb34a2c1c1b98f3p1f1829jsn9a2bff845530',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "aaf099303f5b01ed68f9b5a28bb92471";
+
